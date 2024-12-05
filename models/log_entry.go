bb1d4158830dc6de66db4bea6f503a02815a1efd
zhujia <hello@zhujia.dev>
2024-12-05 16:28:14 +0800

    feat(log): 使用zap替换go-kit log

--- models/log_entry.go
+++ models/log_entry.go
@@ -23,6 +23,7 @@ import (
 
 // These constants identify the log levels in order of increasing severity.
 const (
+	TraceLog = "TRACE"
 	DebugLog = "DEBUG"
 	InfoLog  = "INFO"
 	WarnLog  = "WARN"
@@ -75,7 +76,7 @@ func (le *LogEntry) UnmarshalJSON(data []byte) error {
 // Validate satisfies the Validator interface
 func (le LogEntry) Validate() (bool, errors.EdgeX) {
 	if !le.isValidated {
-		logLevels := []string{DebugLog, InfoLog, WarnLog, ErrorLog}
+		logLevels := []string{TraceLog, DebugLog, InfoLog, WarnLog, ErrorLog}
 		for _, name := range logLevels {
 			if name == le.Level {
 				return true, nil
